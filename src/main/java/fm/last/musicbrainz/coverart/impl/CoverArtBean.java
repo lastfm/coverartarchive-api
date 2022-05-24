9cfc0e9b613866752b764f2c9c403938b2c7808f
Adrian Woodhead <massdosage@gmail.com>
2022-05-24 18:27:58 +0100

    Update various versions (#11)
    * update various versions
    * add workflow

--- src/main/java/fm/last/musicbrainz/coverart/impl/CoverArtBean.java
+++ src/main/java/fm/last/musicbrainz/coverart/impl/CoverArtBean.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012 Last.fm
+ * Copyright (C) 2012-2022 Last.fm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
